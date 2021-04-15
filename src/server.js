bf69a90460d06d284d3db87281113b55fa3d4314
Graziani Zanfolin <54035931+zanfolin@users.noreply.github.com>
2021-04-15 07:50:51 -0300

    Config of nodemonConfig of package.json file

--- src/server.js
+++ src/server.js
@@ -11,5 +11,5 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 server.listen(8080, () => {
-  console.log("JSON Server is running");
+  console.log("JSON Server esta executando");
 });
